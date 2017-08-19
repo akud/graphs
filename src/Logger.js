512b4da01155e938ff0c06b4304035902c14fc59
Alex Kudlick <alex@rescale.com>
2017-08-19 08:54:25 -0700

    Logger - add timestamp

--- src/Logger.js
+++ src/Logger.js
@@ -4,13 +4,13 @@ function Logger() {
 
 Logger.prototype = {
   debug: function(msg, objs) {
-    this._log.apply(this, ['DEBUG'].concat(Array.prototype.splice.call(arguments, 0)));
+    this._log.apply(this, [new Date().getTime(), 'DEBUG'].concat(Array.prototype.splice.call(arguments, 0)));
   },
   warn: function(msg, objs) {
-    this._log.apply(this, ['WARN'].concat(Array.prototype.splice.call(arguments, 0)));
+    this._log.apply(this, [new Date().getTime(), 'WARN'].concat(Array.prototype.splice.call(arguments, 0)));
   },
   error: function(msg, objs) {
-    this._log.apply(this, ['ERROR'].concat(Array.prototype.splice.call(arguments, 0)));
+    this._log.apply(this, [new Date().getTime(), 'ERROR'].concat(Array.prototype.splice.call(arguments, 0)));
   },
 
   _log: function() {
