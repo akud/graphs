c26f5b3e5735106d44421bfbb663c9e4483602cb
Alex Kudlick <alex@rescale.com>
2017-10-24 15:53:03 -0700

    test utils - add method to reset whole spy object

--- test/utils/spy-obj.js
+++ test/utils/spy-obj.js
@@ -2,21 +2,31 @@ var createSpy = require('expect').createSpy;
 
 createSpyObjectWith = function() {
   var obj = {};
+  var spies = [];
   Array.prototype.forEach.call(arguments, function(fnName) {
     if (typeof fnName === 'object') {
+      var spy = null;;
       var spec = fnName;
       Object.keys(spec).forEach(function(key) {
         var value = spec[key];
         if (key.endsWith('.returnValue')) {
-          obj[key.split('.')[0]] = createSpy().andReturn(value === 'this' ? obj : value);
+          spy = createSpy().andReturn(value === 'this' ? obj : value);
+          obj[key.split('.')[0]] = spy;
         } else {
           obj[key] = value;
         }
       });
     } else {
-      obj[fnName] = createSpy();
+      spy = createSpy();
+      obj[fnName] = spy;
+    }
+    if (spy) {
+      spies.push(spy);
     }
   });
+  obj.reset = obj.reset || function() {
+    spies.forEach(function(s) { s.reset() });
+  }
   return obj;
 };
 
